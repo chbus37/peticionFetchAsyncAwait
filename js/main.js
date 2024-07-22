381f7700ee6c0b4e24fe059e3f71d1104829072b
Christian Bustos <christianbus37@gmail.com>
2024-07-22 09:14:54 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      return res.ok ? res.json() : Promise.reject(res);
+    })
+    .then((json) => {
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      let message = err.statusText || "Ocurrio un error";
+      $fetch.innerHTML = `Error ${err.status}:${message}`;
+    })
+    .finally(() =>
+      console.log("Esto se ejecutara independientemente de la promesa Fetch")
+    );
+})();
+
+(() => {
+  const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+  async function getData() {
+    try {
+      let res = await fetch("https://jsonplaceholder.typicode.com/users"),
+        json = await res.json();
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetchAsync.appendChild($fragment);
+    } catch (err) {}
+  }
+
+  getData();
+})();
