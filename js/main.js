01454299907e52d91169474a9f3b67b3f1d6298c
Christian Bustos <christianbus37@gmail.com>
2024-07-22 09:22:00 -0300

    fetch con async await terminado

--- js/main.js
+++ js/main.js
@@ -1,30 +1,4 @@
 (() => {
-  const $fetch = document.getElementById("fetch"),
-    $fragment = document.createDocumentFragment();
-
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((res) => {
-      return res.ok ? res.json() : Promise.reject(res);
-    })
-    .then((json) => {
-      json.forEach((el) => {
-        const $li = document.createElement("li");
-        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
-        $fragment.appendChild($li);
-      });
-
-      $fetch.appendChild($fragment);
-    })
-    .catch((err) => {
-      let message = err.statusText || "Ocurrio un error";
-      $fetch.innerHTML = `Error ${err.status}:${message}`;
-    })
-    .finally(() =>
-      console.log("Esto se ejecutara independientemente de la promesa Fetch")
-    );
-})();
-
-(() => {
   const $fetchAsync = document.getElementById("fetch-async"),
     $fragment = document.createDocumentFragment();
 
@@ -33,6 +7,10 @@
       let res = await fetch("https://jsonplaceholder.typicode.com/users"),
         json = await res.json();
 
+      if (!res.ok) {
+        throw { status: res.status, statusText: res.statusText };
+      }
+
       json.forEach((el) => {
         const $li = document.createElement("li");
         $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
@@ -40,7 +18,10 @@
       });
 
       $fetchAsync.appendChild($fragment);
-    } catch (err) {}
+    } catch (err) {
+      let message = err.statusText || "Ocurrio un error";
+      $fetchAsync.innerHTML = `Error ${err.status}:${message}`;
+    }
   }
 
   getData();
